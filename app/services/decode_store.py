d603c2a291139b2ea28702d98ef778a8ee14f945
vegetoc <amin.alami@epita.fr>
2024-04-11 15:30:36 +0200

    json to text

--- app/services/decode_store.py
+++ app/services/decode_store.py
@@ -2,6 +2,8 @@ import base64
 import psycopg2
 from psycopg2.extras import execute_values
 import msgpack
+from flask import Flask, request, jsonify
+import json  
 
 # Configurations de la base de données (à remplacer par vos valeurs)
 DATABASE = "nom_de_votre_base_de_donnees"
@@ -10,26 +12,26 @@ PASSWORD = "votre_mot_de_passe"
 HOST = "localhost"  # ou l'adresse du service dans docker-compose
 PORT = "5432"
 
+
 def decode_and_store(encoded_data, table_name):
-    # First, decode from Base64
+    # Premièrement, décoder depuis Base64
     base64_decoded_bytes = base64.b64decode(encoded_data)
 
-    # Then, unpack the MessagePack data
+    # Ensuite, décompresser les données MessagePack
     decoded_data = msgpack.unpackb(base64_decoded_bytes)
-
     
-    # Dans cet exemple, on suppose que decoded_str est simplement un texte à stocker
-    data_to_insert = decoded_data
+    # Convertir les données JSON en texte
+    data_text = json.dumps(decoded_data)
     
-    # Insérer les données dans la base de données PostgreSQL
+    # Insérer les données textuelles dans la base de données PostgreSQL
     conn = psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT)
     cur = conn.cursor()
     
     # La requête SQL dépendra de la structure de votre table et des données
     insert_query = f"INSERT INTO {table_name} (column_name) VALUES %s;"
     
-    # Exécuter la requête d'insertion
-    execute_values(cur, insert_query, [(data_to_insert,)])
+    # Exécuter la requête d'insertion avec les données sous forme de texte
+    execute_values(cur, insert_query, [(data_text,)])
     
     # Valider la transaction
     conn.commit()
