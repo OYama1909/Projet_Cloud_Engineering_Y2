68939eba8757d7893b5081e6a216f278feb7ca50
vegetoc <amin.alami@epita.fr>
2024-04-11 12:54:11 +0200

    enfin sa marche

--- app/main.py
+++ app/main.py
@@ -1,21 +1,28 @@
-from flask import Flask, request, jsonify  
+from flask import Flask, request, jsonify
 import msgpack
+import base64
+
 
 app = Flask(__name__)
 
-
-@app.route('/recevie', methods=['POST'])
+@app.route('/receive', methods=['POST'])
 def receive_data():
     # Vérifier si les données reçues sont au format MsgPack
-    if request.content_type == 'application/msgpack':
+    if request.content_type == 'application/json':
         data = request.data
         # Décoder les données MsgPack
-        decoded_data = msgpack.unpackb(data, raw=False)
-        # Afficher les données décodées
-        print(decoded_data)
-        return jsonify({"status": "success", "message": "Data received and decoded successfully."}), 200
+        try:
+            # First, decode from Base64
+            base64_decoded_bytes = base64.b64decode(data)
+            # Then, unpack the MessagePack data
+            decoded_data = msgpack.unpackb(base64_decoded_bytes)
+            # Afficher les données décodées
+            print(decoded_data)
+            return jsonify({"status": "success", "message": "Data received and decoded successfully."}), 200
+        except msgpack.ExtraData as e:
+            return jsonify({"status": "error", "message": "Extra data received."}), 400
     else:
-        return jsonify({"status": "error", "message": "Invalid content type. Please send MsgPack data."}), 415
+       return jsonify({"status": "error", "message": "Invalid content type. Please send MsgPack data."}), 415
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
